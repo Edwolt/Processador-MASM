4dff7535c879e365b770fc9f281e8fec25a3e7fb
Edws <eduardos.rocha17@gmail.com>
2020-06-10 12:11:53 -0300

    [untested] Creating parser
    Creating  the code parser
    It's a finite state machine
    Rewriting main.c (memory.h and memory.c doesn't exist anymore)

--- Assembler/main.c
+++ Assembler/main.c
@@ -1,9 +1,9 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
-#include "memory.h"
+#include "parser.h"
 
 // -m MIF file
 // -b binary file
@@ -21,50 +21,9 @@ int main(int argc, char const *argv[]) {
         memory[i] = 0;
     }
 
-    FILE *source = fopen(argv[1], "r");
     FILE *dest = fopen(argv[2], "wb");
 
-    char name[20];
-    int instruction;
-
-    i = 0;
-    while (fscanf(source, " %s", name) == 1) {
-        if (strcmp(name, "j")) {
-            instruction = OP_J << 12;
-        } else if (strcmp(name, "cmp") == 1) {
-            instruction = OP_CMP << 12;
-        } else if (strcmp(name, "store") == 1) {
-            instruction = OP_STORE << 12;
-        } else if (strcmp(name, "load") == 1) {
-            instruction = OP_LOAD << 12;
-        } else if (strcmp(name, "in") == 1) {
-            instruction = OP_INOUT << 12;
-        } else if (strcmp(name, "out") == 1) {
-            instruction = OP_INOUT << 12;
-        } else if (strcmp(name, "set") == 1) {
-            instruction = OP_SET << 12;
-        } else if (strcmp(name, "move") == 1) {
-            instruction = OP_MOVE << 12;
-        } else if (strcmp(name, "add") == 1) {
-            instruction = OP_ADD << 12;
-        } else if (strcmp(name, "sub") == 1) {
-            instruction = OP_SUB << 12;
-        } else if (strcmp(name, "mul") == 1) {
-            instruction = OP_MUL << 12;
-        } else if (strcmp(name, "div") == 1) {
-            instruction = OP_DIV << 12;
-        } else if (strcmp(name, "and") == 1) {
-            instruction = OP_AND << 12;
-        } else if (strcmp(name, "or") == 1) {
-            instruction = OP_OR << 12;
-        } else if (strcmp(name, "xor") == 1) {
-            instruction = OP_XOR << 12;
-        } else if (strcmp(name, "not") == 1) {
-            instruction = OP_NOT << 12;
-        }
-        memory[i] = instruction;
-        i++;
-    }
+    // TODO parse the assembly code
 
     for (i = TAM_MEM; i > 0; i--) {
         if (memory[i] != 0) break;
@@ -76,6 +35,5 @@ int main(int argc, char const *argv[]) {
         fwrite(memory + j, sizeof(int), 1, dest);
     }
 
-    fclose(source);
     fclose(dest);
 }
--- Assembler/parser.c
+++ Assembler/parser.c
@@ -1,23 +1,75 @@
 #include "parser.h"
 
+#define TAM_COMMAND 100
+
 enum State {
-    END_ARGUMENT = 0,
-    EXPECT_REGISTER1 = 1,
-    EXPECT_REGISTER2 = 2,
-    EXPECT_REGISTER3 = 3,
-    EXPECT_VALUE,
-    NEXT_CHUNCK,
-    EXPECT_NEWLINE,
-    END_FILE,
+    START,
+    COMMENT,
+    DECIMAL,
+    POSITIVE_DECIMAL,
+    NEGATIVE_DECIMAL,
+    BINARY,
+    HEXADECIMAL,
+    LABEL,
+    COMMAND,
+    END
 };
 
+enum Opcode {
+    OP_J = 0xb0000,
+    OP_CMP = 0xb0001,
+    OP_STORE = 0xb0010,
+    OP_LOAD = 0xb0011,
+    OP_INOUT = 0xb0100,
+    OP_SET = 0xb0101,
+    OP_MOVE = 0xb0110,
+    OP_ADD = 0xb0111,
+    OP_SUB = 0xb1000,
+    OP_MUL = 0xb1001,
+    OP_DIV = 0xb1010,
+    OP_SHIFT = 0xb1011,
+    OP_AND = 0xb1100,
+    OP_OR = 0xb1101,
+    OP_XOR = 0xb1110,
+    OP_NOT = 0xb1111,
+};
+
+/*
+The parser is a finite state machine that parse the assembly code
+*/
 struct _Parser {
     FILE* file;
     enum State state;
     int line;
 };
 
-#define TAM_COMMAND 25
+static bool isSpace(char c) {
+    return c == '\t' ||
+           c == '\v' ||
+           c == ' ';
+}
+
+static bool isEnter(char c) {
+    return c == '\n' ||
+           c == '\f' ||
+           c == '\r';
+}
+
+static bool isDecimal(char c) {
+    return 0 <= c - '0' && c - '0' < 10;
+}
+
+static Command* createCommand() {
+    Command* command = malloc(sizeof(Command));
+    if (!command) return NULL;
+
+    command->what = COMMAND_CONST;
+    command->command = 0;
+    command->label = NULL;
+    command->nextLine = 0;
+
+    return command;
+}
 
 Parser* createParser(char* path) {
     Parser* parser = malloc(sizeof(Parser));
@@ -29,8 +81,10 @@ Parser* createParser(char* path) {
         return NULL;
     }
 
-    parser->state = NEXT_CHUNCK;
+    parser->state = START;
     parser->line = 1;
+
+    return parser;
 }
 
 void deleteParser(Parser* parser) {
@@ -48,22 +102,157 @@ void deleteCommand(Command* command) {
 }
 
 Command* parser_nextChunck(Parser* parser) {
-    if (!parser) return;
+    if (!parser) return NULL;
+
+    Command* command = createCommand();
+    if (!command) return NULL;
+
+    if (parser->state == END) {
+        command->what = COMMAND_ENDFILE;
+        return command;
+    }
+
+    command->label = malloc(TAM_COMMAND * sizeof(char));
 
     int i;
     char c;
 
-    Command* command = malloc(sizeof(command));
-    command->label = malloc(TAM_COMMAND * sizeof(char));
-
-    for (i = 0; i < TAM_COMMAND; i++) {
-        fscanf("%c", &c);
-        if (c == ':') {
+    while (true) {
+        if (parser->state == LABEL) {
             command->what = COMMAND_LABEL;
-            command->label[i] = '\0';
+            parser->state = START;
             return command;
         }
-        if (c == ' ') {
+
+        fscanf(parser->file, "%c", &c);
+        if (feof(parser->file)) {
+            command->what = COMMAND_ENDFILE;
+            free(command->label);
+            parser->state = END;
+
+            return command;
+        }
+
+        switch (parser->state) {
+            case START:
+                if (isEnter(c)) {
+                    parser->line++;
+                    parser->state = START;
+                } else if (isSpace(c)) {
+                    parser->state = START;
+                } else if (c == ';') {
+                    parser->state = COMMENT;
+                } else if (c == 'x') {
+                    parser->state = HEXADECIMAL;
+                } else if (c == 'b') {
+                    parser->state = BINARY;
+                } else if (c == '+') {
+                    parser->state = POSITIVE_DECIMAL;
+                } else if (c == '-') {
+                    parser->state = NEGATIVE_DECIMAL;
+                } else if (c == ':') {
+                    printf("Line %d: Empty label\n", parser->line);
+                } else if (isDecimal(c)) {
+                    parser->state = DECIMAL;
+                } else {  // Common char
+                    command->label[i++] = c;
+                    parser->state = COMMAND;
+                }
+                break;
+
+            case COMMENT:
+                if (isEnter(c)) {
+                    parser->state = START;
+                    parser->line++;
+                }
+                break;
+
+            case DECIMAL:
+                if (isEnter(c)) {
+                    if (i > 5) {
+                        printf("Line %d: This number is too large\n", parser->line);
+                    }
+                    uint32_t num = atoi(command->label);
+                    if (num > UINT16_MAX) {
+                        printf("Line %d: This number is too large\n", parser->line);
+                    }
+                    free(command->label);
+                    command->label = NULL;
+                    command->what = COMMAND_CONST;
+                    command->command = (uint16_t)num;
+
+                    parser->state = START;
+                    parser->line++;
+
+                    return command;
+                } else if (isSpace(c)) {
+                    if (i > 5) {
+                        printf("Line %d: This number is too large\n", parser->line);
+                    }
+                    uint32_t num = atoi(command->label);
+                    if (num > UINT16_MAX) {
+                        printf("Line %d: This number is too large\n", parser->line);
+                    }
+                    free(command->label);
+                    command->label = NULL;
+                    command->what = COMMAND_CONST;
+                    command->command = (uint16_t)num;
+
+                    parser->state = START;
+
+                    return command;
+                } else if (c == ';') {
+                    if (i > 5) {
+                        printf("Line %d: This number is too large\n", parser->line);
+                    }
+                    uint32_t num = atoi(command->label);
+                    if (num > UINT16_MAX) {
+                        printf("Line %d: This number is too large\n", parser->line);
+                    }
+                    free(command->label);
+                    command->label = NULL;
+                    command->what = COMMAND_CONST;
+                    command->command = (uint16_t)num;
+
+                    parser->state = COMMENT;
+
+                    return command;
+                } else if (isDecimal(c)) {
+                    command->label[i++] = c;
+                } else if (c == ':') {
+                    parser->state = LABEL;
+                }
+                break;
+
+            case POSITIVE_DECIMAL:
+                // TODO Looks like DECIMAL
+                break;
+
+            case NEGATIVE_DECIMAL:
+                // TODO Looks like DECIMAL
+                break;
+
+            case BINARY:
+                // TODO Looks like DECIMAL
+                break;
+
+            case HEXADECIMAL:
+                // TODO Looks like DECIMAL
+                break;
+
+            case COMMAND:
+                // TODO
+                break;
+
+            default:
+                printf("The assembler doesn't work");
+                free(command->label);
+                command->what = COMMAND_ENDFILE;
+                parser->state = END;
         }
     }
+    printf("The assembler doesn't work");
+    free(command->label);
+    command->what = COMMAND_ENDFILE;
+    parser->state = END;
 }
