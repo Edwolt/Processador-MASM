32cb7cc2d5913f164d89a96839ce1bff0ba0149e
Edws <eduardos.rocha17@gmail.com>
2020-06-24 14:57:58 -0300

    Changing the messages

--- Assembler/util.hpp
+++ Assembler/util.hpp
@@ -4,12 +4,12 @@
 
 extern bool debug;
 
-#define cerror   cout << "\033[1;31m[Erro]\033[0m "
-#define cwarning cout << "\033[1;35m[Warn]\033[0m "
-#define cbug     cout << "\033[1;41m!!\033[0m "
-#define cdebug   if (debug) cout << "\033[1;32m??\033[0m "
-#define cdebugr  if (debug) cout << "\033[1;36m<<\033[0m "
+#define cerror   cout << "\033[1;31m[ERROR]\033[0m "
+#define cwarning cout << "\033[1;35m[WARN ]\033[0m "
+#define cbug     cout << "\033[1;41m[ BUG ]\033[0m "
+#define cdebug   if (debug) cout << "\033[1;32m[DEBUG]\033[0m "
+#define cdebugr  if (debug) cout << "\033[1;36m[DEBUG]\033[0m "
 #define cass     cout << "\033[1;33m>>\033[0m "
 
-#define lerror(line)   cerror << "\033[1mLine " << (line) << ":\033[0m "
-#define lwarning(line) cwarning << "\033[1mLine " << (line) << ":\033[0m "
+#define lerror(line)   cout << "\033[1;31m[ERROR] Line " << (line) << ":\033[0m "
+#define lwarning(line) cout << "\033[1;35m[WARN ] Line " << (line) << ":\033[0m "
