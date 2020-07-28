6474213f204a74ca3d000d8e1a8a1ce97431d9c2
Edws <eduardos.rocha17@gmail.com>
2020-07-27 21:15:00 -0300

    Starting the development od Simulator

---
+++ Simulator/files.cpp
@@ -0,0 +1,5 @@
+#include "files.hpp"
+
+vector<u16> readBinary() {
+    return vector<u16>(0, 1);
+}
---
+++ Simulator/files.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "util.hpp"
+
+using namespace std;
+
+vector<u16> readBinary(string path);
