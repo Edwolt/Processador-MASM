4489c01b99979ce18a060fdcc5c0e31dabff2105
Edws <eduardos.rocha17@gmail.com>
2020-07-27 22:37:35 -0300

    Read file

--- Simulator/files.cpp
+++ Simulator/files.cpp
@@ -1,5 +1,28 @@
 #include "files.hpp"
 
-vector<u16> readBinary() {
-    return vector<u16>(0, 1);
+inline static u16 strToNum(char str[2]) {
+    unsigned char *aux = (unsigned char *)str;
+    return (aux[0] << 8) | aux[1];
+}
+
+vector<u16> readBinary(string path) {
+    vector<u16> memory(MEM_DEPTH, 0);
+
+    fstream file(path, ios::in | ios::binary);
+    if (!file) {
+        cout << "Can't open the file: " << path << endl;
+        return memory;
+    }
+
+    char aux[2];
+    file.read(aux, 2);
+
+    u16 n = strToNum(aux);
+
+    for (u16 i = 0; i < n; i++) {
+        file.read(aux, 2);
+        memory[i] = strToNum(aux);
+    }
+
+    return memory;
 }
