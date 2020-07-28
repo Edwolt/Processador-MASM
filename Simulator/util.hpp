92bb06fd13b578048e7abd5e14d1440830558b3c
Edws <eduardos.rocha17@gmail.com>
2020-07-28 17:16:09 -0300

    Instruction execution (it doesn't compile)

--- Simulator/util.hpp
+++ Simulator/util.hpp
@@ -3,5 +3,19 @@
 #include <cstdint>
 
 typedef uint16_t u16;
+typedef uint32_t u32;
 
 const int MEM_DEPTH = 1 << 16;
+
+template <typename T, typename U, typename V>
+struct trio {
+    T first;
+    U second;
+    V third;
+
+    trio(T first, U second, V third) {
+        this->first = first;
+        this->second = second;
+        this->third = third;
+    }
+};
