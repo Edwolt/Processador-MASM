ddc4cce831bb62a15c412e81db9020e918133af7
Edws <eduardos.rocha17@gmail.com>
2020-07-27 22:37:17 -0300

    Details on assembler

--- Assembler/const.hpp
+++ Assembler/const.hpp
@@ -15,5 +15,6 @@ bool isNum(string str);
 bool isInvalidNum(string str);
 u16 evalNum(int line, string str);
 u16 evalNumImm(int line, string str);
+
 pair<u16, u16> evalArr(int line, string str);
 vector<u16> evalStr(int line, string str);
